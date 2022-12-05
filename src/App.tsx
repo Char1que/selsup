6bee0f6b1ed8290e7dbc5ac67d94ec6c6f5589ef
Char1que <char1k59@mail.ru>
2022-12-05 11:40:36 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Param {
+  id: number;
+  name: string;
+  type?: string;
 }
 
-export default App;
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+
+interface Model {
+  paramValues: ParamValue[];
+  colors: Color[];
+}
+
+interface Color {}
+
+interface Props {
+  params: Param[];
+  model: Model;
+}
+
+interface State {
+  model: Model;
+}
+
+const params: Param[] = [
+  { id: 1, name: "Назначение" },
+  { id: 2, name: "Длина" },
+];
+
+const model: Model = {
+  colors: [],
+  paramValues: [
+    { paramId: 1, value: "повседневное" },
+    { paramId: 2, value: "макси" },
+  ],
+};
+
+class ParamEditor extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.getModel = this.getModel.bind(this);
+    this.setModel = this.setModel.bind(this);
+    this.state = {model};
+  }
+
+  public getModel(e: React.FormEvent<HTMLFormElement>): Model {
+    e.preventDefault();
+    console.log(this.state.model);
+    return this.state.model;
+  }
+
+  public setModel(e: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState((prevState: State) => {
+      const model = prevState.model;
+      const propId: number = e.target.name as unknown as number;
+      model.paramValues[propId - 1].value = e.target.value;
+      return { model };
+    });
+  };
+
+  public alertFunction(): void {
+    alert('Модель была получена в консоли!')
+  }
+
+  render() {
+    return (
+      <form className="editor" onSubmit={this.getModel}>
+        {params.map((item: { id: number; name: string }) => (
+          <div key={item.id}>
+            <div >{item.name}</div>
+            <input name={`${item.id}`} autoFocus={true} value={this.state.model.paramValues[item.id - 1].value} onChange={this.setModel}/>
+          </div>
+        ))}
+        <button onClick={this.alertFunction}>
+          Получить модель
+        </button>
+      </form>
+    );
+  }
+}
+
+export default function App () {
+  return <ParamEditor model={model} params={params} />;
+};
